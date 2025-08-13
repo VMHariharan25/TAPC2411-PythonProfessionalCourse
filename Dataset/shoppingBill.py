1b608c479199f3d5a4aba6f1a243ee5b3ddca9ac
Hariharan Velmurugan <hariharanvels@velsinfoway.com>
2025-08-13 21:54:38 +0530

    With DS

---
+++ Dataset/shoppingBill.py
@@ -0,0 +1,355 @@
+shoppingBill = {
+    1: {
+        "Shop": "SV Stores",
+        "Customer": "Srivatsan",
+        "Items": [
+            {"Item": "Carrots", "Category": "Vegetables/Food", "Qty": 1.5, "Price": 50, "Cost": 75},
+            {"Item": "Soap", "Category": "Toiletries", "Qty": 4, "Price": 32, "Cost": 128},
+            {"Item": "Tomatoes", "Category": "Vegetables/Food", "Qty": 2, "Price": 40, "Cost": 80},
+            {"Item": "Bananas", "Category": "Vegetables/Food", "Qty": 8, "Price": 8, "Cost": 64},
+            {"Item": "Socks", "Category": "Footwear/Apparel", "Qty": 3, "Price": 56, "Cost": 168},
+            {"Item": "Curd", "Category": "Dairy/Food", "Qty": 0.5, "Price": 32, "Cost": 16},
+            {"Item": "Milk", "Category": "Dairy/Food", "Qty": 1.5, "Price": 24, "Cost": 36}
+        ],
+        "Total": 567
+    },
+    2: {
+        "Shop": "Big Bazaar",
+        "Customer": "Sudeep",
+        "Items": [
+            {"Item": "Baked Beans", "Category": "Canned/Food", "Qty": 1, "Price": 125, "Cost": 125},
+            {"Item": "Chicken Wings", "Category": "Meat/Food", "Qty": 0.5, "Price": 600, "Cost": 300},
+            {"Item": "Cocoa powder", "Category": "Canned/Food", "Qty": 1, "Price": 160, "Cost": 160},
+            {"Item": "Capsicum", "Category": "Vegetables/Food", "Qty": 0.8, "Price": 180, "Cost": 144},
+            {"Item": "Tie", "Category": "Apparel", "Qty": 2, "Price": 390, "Cost": 780},
+            {"Item": "Clips", "Category": "Household", "Qty": 0.5, "Price": 32, "Cost": 16}
+        ],
+        "Total": 1525
+    },
+    3: {
+        "Shop": "Sun General",
+        "Customer": "Srivatsan",
+        "Items": [
+            {"Item": "Batteries", "Category": "Utilities", "Qty": 6, "Price": 14, "Cost": 84},
+            {"Item": "USB Cable", "Category": "Electronics", "Qty": 1, "Price": 85, "Cost": 85},
+            {"Item": "Ball Pens", "Category": "Stationery", "Qty": 5, "Price": 12, "Cost": 60},
+            {"Item": "Onions", "Category": "Vegetables/Food", "Qty": 1.25, "Price": 100, "Cost": 125}
+        ],
+        "Total": 354
+    },
+    4: {
+        "Shop": "SV Stores",
+        "Customer": "Akshaya",
+        "Items": [
+            {"Item": "Face Wash", "Category": "Toiletries", "Qty": 1, "Price": 89, "Cost": 89},
+            {"Item": "Shampoo", "Category": "Toiletries", "Qty": 1, "Price": 140, "Cost": 140},
+            {"Item": "Tomatoes", "Category": "Vegetables/Food", "Qty": 1, "Price": 98, "Cost": 98},
+            {"Item": "Bananas", "Category": "Fruits/Food", "Qty": 4, "Price": 8, "Cost": 32},
+            {"Item": "Milk", "Category": "Dairy/Food", "Qty": 1, "Price": 24, "Cost": 24},
+            {"Item": "Biscuits", "Category": "Packed/Food", "Qty": 2, "Price": 22, "Cost": 44},
+            {"Item": "Maggi", "Category": "Packed/Food", "Qty": 1, "Price": 85, "Cost": 85},
+            {"Item": "Horlicks", "Category": "Packed/Food", "Qty": 1, "Price": 270, "Cost": 270},
+            {"Item": "Chips", "Category": "Packed/Food", "Qty": 1, "Price": 20, "Cost": 20},
+            {"Item": "Chocolates", "Category": "Packed/Food", "Qty": 4, "Price": 10, "Cost": 40},
+            {"Item": "Cereal", "Category": "Packed/Food", "Qty": 1, "Price": 220, "Cost": 220},
+            {"Item": "Handwash", "Category": "Toiletries", "Qty": 1, "Price": 139, "Cost": 139},
+            {"Item": "Air freshener", "Category": "Toiletries", "Qty": 2, "Price": 70, "Cost": 140}
+        ],
+        "Total": 1341
+    },
+    5: {
+        "Shop": "Big Bazaar",
+        "Customer": "Akshaya",
+        "Items": [
+            {"Item": "Trousers", "Category": "Women/Apparel", "Qty": 2, "Price": 870, "Cost": 1740},
+            {"Item": "Shirts", "Category": "Women/Apparel", "Qty": 1, "Price": 1350, "Cost": 1350},
+            {"Item": "Detergent", "Category": "Household", "Qty": 0.5, "Price": 270, "Cost": 135},
+            {"Item": "Tee shirts", "Category": "Women/Apparel", "Qty": 4, "Price": 220, "Cost": 880},
+            {"Item": "Instant Noodles", "Category": "Canned/Food", "Qty": 3, "Price": 23, "Cost": 69}
+        ],
+        "Total": 4174
+    },
+    6: {
+        "Shop": "Sun General",
+        "Customer": "Rajesh",
+        "Items": [
+            {"Item": "Notebooks", "Category": "Stationery", "Qty": 3, "Price": 20, "Cost": 60},
+            {"Item": "Apples", "Category": "Fruits/Food", "Qty": 6, "Price": 20, "Cost": 120},
+            {"Item": "Pears", "Category": "Fruits/Food", "Qty": 4, "Price": 30, "Cost": 120},
+            {"Item": "Chart Paper", "Category": "Stationery", "Qty": 2, "Price": 22, "Cost": 44},
+            {"Item": "Ruler", "Category": "Stationery", "Qty": 1, "Price": 10, "Cost": 10}
+        ],
+        "Total": 378
+    },
+    7: {
+        "Shop": "SV Stores",
+        "Customer": "Advaith",
+        "Items": [
+            {"Item": "Milk", "Category": "Dairy/Food", "Qty": 2, "Price": 24, "Cost": 48},
+            {"Item": "Bread", "Category": "Packed/Food", "Qty": 1, "Price": 30, "Cost": 30},
+            {"Item": "Eggs", "Category": "Food", "Qty": 1, "Price": 45, "Cost": 45}
+        ],
+        "Total": 123
+    },
+    8: {
+        "Shop": "Big Bazaar",
+        "Customer": "Advaith",
+        "Items": [
+            {"Item": "Trousers", "Category": "Men/Apparel", "Qty": 2, "Price": 950, "Cost": 1900},
+            {"Item": "Basa Fish", "Category": "Meal/Food", "Qty": 1, "Price": 350, "Cost": 350},
+            {"Item": "Boxers", "Category": "Men/Apparel", "Qty": 4, "Price": 160, "Cost": 640},
+            {"Item": "Face Wash", "Category": "Toiletries", "Qty": 1, "Price": 72, "Cost": 72},
+            {"Item": "Slippers", "Category": "Footwear/Apparel", "Qty": 1, "Price": 170, "Cost": 170}
+        ],
+        "Total": 3132
+    },
+    9: {
+        "Shop": "Sun General",
+        "Customer": "Aparna",
+        "Items": [
+            {"Item": "Mosquito Coil", "Category": "Household", "Qty": 2, "Price": 24, "Cost": 48},
+            {"Item": "Bananas", "Category": "Fruits/Food", "Qty": 6, "Price": 5, "Cost": 30},
+            {"Item": "Ball Pens", "Category": "Stationery", "Qty": 4, "Price": 12, "Cost": 48},
+            {"Item": "Paper Clips", "Category": "Stationery", "Qty": 1, "Price": 60, "Cost": 60}
+        ],
+        "Total": 186
+    },
+    10: {
+        "Shop": "SV Stores",
+        "Customer": "Akhil",
+        "Items": [
+            {"Item": "Bread", "Category": "Packed/Food", "Qty": 1, "Price": 30, "Cost": 30},
+            {"Item": "Biscuits", "Category": "Packed/Food", "Qty": 3, "Price": 22, "Cost": 66}
+        ],
+        "Total": 96
+    },
+    11: {
+        "Shop": "Big Bazaar",
+        "Customer": "Mohith",
+        "Items": [
+            {"Item": "Lindt", "Category": "Chocolate/Food", "Qty": 1, "Price": 125, "Cost": 125},
+            {"Item": "Socks", "Category": "Footwear/Apparel", "Qty": 1, "Price": 120, "Cost": 120}
+        ],
+        "Total": 595
+    },
+    12: {
+        "Shop": "Sun General",
+        "Customer": "Vignesh",
+        "Items": [
+            {"Item": "Phone Charger", "Category": "Utilities", "Qty": 1, "Price": 230, "Cost": 230},
+            {"Item": "Razor Blades", "Category": "Grooming", "Qty": 1, "Price": 12, "Cost": 12},
+            {"Item": "Razor", "Category": "Grooming", "Qty": 1, "Price": 45, "Cost": 45},
+            {"Item": "Shaving Lotion", "Category": "Grooming", "Qty": 0.8, "Price": 180, "Cost": 144},
+            {"Item": "Earphones", "Category": "Electronics", "Qty": 1, "Price": 210, "Cost": 210},
+            {"Item": "Pencils", "Category": "Stationery", "Qty": 3, "Price": 5, "Cost": 15}
+        ],
+        "Total": 656
+    },
+    13: {
+        "Shop": "SV Stores",
+        "Customer": "Abhinav",
+        "Items": [
+            {"Item": "Chocolates", "Category": "Packed/Food", "Qty": 1, "Price": 10, "Cost": 10},
+            {"Item": "Cereal", "Category": "Packed/Food", "Qty": 1, "Price": 220, "Cost": 220},
+            {"Item": "Bananas", "Category": "Fruits/Food", "Qty": 6, "Price": 8, "Cost": 48},
+            {"Item": "Tomatoes", "Category": "Vegetables/Food", "Qty": 1, "Price": 40, "Cost": 40},
+            {"Item": "Curd", "Category": "Dairy/Food", "Qty": 1, "Price": 32, "Cost": 32},
+            {"Item": "Milk", "Category": "Dairy/Food", "Qty": 2, "Price": 24, "Cost": 48},
+            {"Item": "Horlicks", "Category": "Packed/Food", "Qty": 1, "Price": 270, "Cost": 270},
+            {"Item": "Plates", "Category": "Household", "Qty": 4, "Price": 45, "Cost": 180},
+            {"Item": "Eggs", "Category": "Food", "Qty": 1, "Price": 45, "Cost": 45}
+        ],
+        "Total": 893
+    },
+    14: {
+        "Shop": "Big Bazaar",
+        "Customer": "Abhinav",
+        "Items": [
+            {"Item": "Shoes", "Category": "Footwear/Apparel", "Qty": 1, "Price": 2700, "Cost": 2700},
+            {"Item": "Polish", "Category": "Footwear/Apparel", "Qty": 1, "Price": 120, "Cost": 120},
+            {"Item": "Socks", "Category": "Footwear/Apparel", "Qty": 2, "Price": 120, "Cost": 240}
+        ],
+        "Total": 3060
+    },
+    15: {
+        "Shop": "Sun General",
+        "Customer": "Abhinav",
+        "Items": [
+            {"Item": "Keyboard", "Category": "Electronics", "Qty": 1, "Price": 780, "Cost": 780},
+            {"Item": "Mouse", "Category": "Electronics", "Qty": 1, "Price": 320, "Cost": 320}
+        ],
+        "Total": 1100
+    },
+    16: {
+        "Shop": "SV Stores",
+        "Customer": "George",
+        "Items": [
+            {"Item": "Cereal", "Category": "Packed/Food", "Qty": 1, "Price": 220, "Cost": 220},
+            {"Item": "Milk", "Category": "Dairy/Food", "Qty": 1, "Price": 24, "Cost": 24},
+            {"Item": "Cupcakes", "Category": "Packed/Food", "Qty": 1, "Price": 25, "Cost": 25},
+            {"Item": "Chocolates", "Category": "Packed/Food", "Qty": 1, "Price": 10, "Cost": 10}
+        ],
+        "Total": 279
+    },
+    17: {
+        "Shop": "Big Bazaar",
+        "Customer": "Radha",
+        "Items": [
+            {"Item": "Broccoli", "Category": "Vegetables/Food", "Qty": 0.5, "Price": 120, "Cost": 60},
+            {"Item": "Chicken Legs", "Category": "Meat/Food", "Qty": 0.5, "Price": 320, "Cost": 160},
+            {"Item": "Basa Fish", "Category": "Meat/Food", "Qty": 1, "Price": 350, "Cost": 350}
+        ],
+        "Total": 798
+    },
+    18: {
+        "Shop": "Sun General",
+        "Customer": "Advaith",
+        "Items": [
+            {"Item": "Pencils", "Category": "Stationery", "Qty": 2, "Price": 5, "Cost": 10},
+            {"Item": "Notebooks", "Category": "Stationery", "Qty": 4, "Price": 20, "Cost": 80},
+            {"Item": "Geometry Box", "Category": "Stationery", "Qty": 1, "Price": 72, "Cost": 72},
+            {"Item": "Graph Book", "Category": "Stationery", "Qty": 1, "Price": 25, "Cost": 25}
+        ],
+        "Total": 187
+    },
+    19: {
+        "Shop": "SV Stores",
+        "Customer": "Ahmed",
+        "Items": [
+            {"Item": "Tomatoes", "Category": "Vegetables/Food", "Qty": 1, "Price": 40, "Cost": 40},
+            {"Item": "Curd", "Category": "Dairy/Food", "Qty": 2, "Price": 32, "Cost": 32},
+            {"Item": "Cupcakes", "Category": "Packed/Food", "Qty": 2, "Price": 25, "Cost": 50},
+            {"Item": "Carrots", "Category": "Vegetables/Food", "Qty": 1.5, "Price": 50, "Cost": 75},
+            {"Item": "Beans", "Category": "Vegetables/Food", "Qty": 1, "Price": 45, "Cost": 45},
+            {"Item": "Onions", "Category": "Vegetables/Food", "Qty": 0.5, "Price": 82, "Cost": 82},
+            {"Item": "Turmeric", "Category": "Packed/Food", "Qty": 1, "Price": 82, "Cost": 82},
+            {"Item": "Ghee", "Category": "Packed/Food", "Qty": 1, "Price": 230, "Cost": 230}
+        ],
+        "Total": 603
+    },
+    20: {
+        "Shop": "Sun General",
+        "Customer": "Suresh",
+        "Items": [
+            {"Item": "Batteries", "Category": "Utilities", "Qty": 2, "Price": 14, "Cost": 28},
+            {"Item": "Tomatoes", "Category": "Vegetables/Food", "Qty": 1.5, "Price": 80, "Cost": 120},
+            {"Item": "Spinach", "Category": "Vegetables/Food", "Qty": 1, "Price": 15, "Cost": 15},
+            {"Item": "Bananas", "Category": "Fruits/Food", "Qty": 4, "Price": 5, "Cost": 20},
+            {"Item": "Mosquito coils", "Category": "Household", "Qty": 1, "Price": 24, "Cost": 24},
+            {"Item": "Guava", "Category": "Fruits/Food", "Qty": 0.4, "Price": 120, "Cost": 48},
+            {"Item": "Potato", "Category": "Vegetables/Food", "Qty": 1.5, "Price": 40, "Cost": 60}
+        ],
+        "Total": 315
+    },
+    21: {
+        "Shop": "SV Stores",
+        "Customer": "Ahmed",
+        "Items": [
+            {"Item": "Tomatoes", "Category": "Vegetables/Food", "Qty": 2, "Price": 40, "Cost": 80},
+            {"Item": "Curd", "Category": "Dairy/Food", "Qty": 2, "Price": 32, "Cost": 64},
+            {"Item": "Cupcakes", "Category": "Packed/Food", "Qty": 3, "Price": 25, "Cost": 75},
+            {"Item": "Carrots", "Category": "Vegetables/Food", "Qty": 0.5, "Price": 50, "Cost": 25},
+            {"Item": "Onions", "Category": "Vegetables/Food", "Qty": 1, "Price": 98, "Cost": 98},
+            {"Item": "Handwash", "Category": "Toiletries", "Qty": 1, "Price": 139, "Cost": 139},
+            {"Item": "Bananas", "Category": "Fruits/Food", "Qty": 12, "Price": 8, "Cost": 96},
+            {"Item": "Eggs", "Category": "Food", "Qty": 1, "Price": 45, "Cost": 45}
+        ],
+        "Total": 622
+    },
+    22: {
+        "Shop": "SV Stores",
+        "Customer": "Neeraja",
+        "Items": [
+            {"Item": "Carrots", "Category": "Vegetables/Food", "Qty": 1, "Price": 50, "Cost": 50},
+            {"Item": "Horlicks", "Category": "Packed/Food", "Qty": 1, "Price": 270, "Cost": 270},
+            {"Item": "Chips", "Category": "Packed/Food", "Qty": 1, "Price": 20, "Cost": 20},
+            {"Item": "Kajal", "Category": "Cosmetics", "Qty": 1, "Price": 180, "Cost": 180},
+            {"Item": "Milk", "Category": "Dairy/Food", "Qty": 3, "Price": 24, "Cost": 72}
+        ],
+        "Total": 592,
+    },
+    23: {
+        "Shop": "SV Stores",
+        "Customer": "Akshaya",
+        "Items": [
+            {"Item": "Curd", "Category": "Dairy/Food", "Qty": 0.5, "Price": 32, "Cost": 16},
+            {"Item": "Butter", "Category": "Dairy/Food", "Qty": 0.2, "Price": 320, "Cost": 64},
+            {"Item": "Milk", "Category": "Dairy/Food", "Qty": 2, "Price": 24, "Cost": 48}
+        ],
+        "Total": 128
+    },
+    24: {
+        "Shop": "SV Stores",
+        "Customer": "Julia",
+        "Items": [
+            {"Item": "Carrots", "Category": "Vegetables/Food", "Qty": 1.5, "Price": 50, "Cost": 75},
+            {"Item": "Bananas", "Category": "Fruits/Food", "Qty": 12, "Price": 8, "Cost": 96},
+            {"Item": "Curd", "Category": "Dairy/Food", "Qty": 3, "Price": 32, "Cost": 96},
+            {"Item": "Milk", "Category": "Dairy/Food", "Qty": 4, "Price": 24, "Cost": 96},
+            {"Item": "Cereal", "Category": "Packed/Food", "Qty": 2, "Price": 220, "Cost": 440},
+            {"Item": "Maggi", "Category": "Packed/Food", "Qty": 1, "Price": 85, "Cost": 85}
+        ],
+        "Total": 888
+    },
+    25: {
+        "Shop": "Sun General",
+        "Customer": "Ahmed",
+        "Items": [
+            {"Item": "Earphones", "Category": "Electronics", "Qty": 1, "Price": 210, "Cost": 210},
+            {"Item": "Phone cover", "Category": "Accessories", "Qty": 1, "Price": 140, "Cost": 140},
+            {"Item": "Dongle", "Category": "Electronics", "Qty": 1, "Price": 790, "Cost": 790},
+            {"Item": "A4 sheets", "Category": "Stationery", "Qty": 200, "Price": 1, "Cost": 200},
+            {"Item": "Ball Pens", "Category": "Stationery", "Qty": 2, "Price": 12, "Cost": 24}
+        ],
+        "Total": 1364
+    },
+    26: {
+        "Shop": "SV Stores",
+        "Customer": "Srivatsan",
+        "Items": [
+            {"Item": "Beans", "Category": "Vegetables/Food", "Qty": 1, "Price": 45, "Cost": 45},
+            {"Item": "Bread", "Category": "Packed/Food", "Qty": 1, "Price": 30, "Cost": 30},
+            {"Item": "Onions", "Category": "Vegetables/Food", "Qty": 0.5, "Price": 98, "Cost": 49},
+            {"Item": "Bananas", "Category": "Fruits/Food", "Qty": 4, "Price": 8, "Cost": 32},
+            {"Item": "Curd", "Category": "Dairy/Food", "Qty": 1, "Price": 32, "Cost": 32},
+            {"Item": "Milk", "Category": "Dairy/Food", "Qty": 3, "Price": 24, "Cost": 72}
+        ],
+        "Total": 276
+    },
+    27: {
+        "Shop": "Sun General",
+        "Customer": "Srivatsan",
+        "Items": [
+            {"Item": "Broom", "Category": "Household", "Qty": 1, "Price": 70, "Cost": 70},
+            {"Item": "Dustpan", "Category": "Household", "Qty": 1, "Price": 45, "Cost": 45},
+            {"Item": "Floor Cleaner", "Category": "Household", "Qty": 1, "Price": 125, "Cost": 125},
+            {"Item": "Tissue Paper", "Category": "Household", "Qty": 2, "Price": 50, "Cost": 100}
+        ],
+        "Total": 340
+    },
+    28: {
+        "Shop": "SV Stores",
+        "Customer": "Neeraja",
+        "Items": [
+            {"Item": "Milk", "Category": "Dairy/Food", "Qty": 3, "Price": 24, "Cost": 72},
+            {"Item": "Chips", "Category": "Packed/Food", "Qty": 1, "Price": 20, "Cost": 20}
+        ],
+        "Total": 92
+    },
+    29: {
+        "Shop": "SV Stores",
+        "Customer": "Vignesh",
+        "Items": [],
+        "Total": None
+    },
+    30: {
+        "Shop": "SV Stores",
+        "Customer": "Ahmed",
+        "Items": [
+            {"Item": "Chocolates", "Category": "Packed/Food", "Qty": 2, "Price": 10, "Cost": 10},
+            {"Item": "Curd", "Category": "Dairy/Food", "Qty": 4, "Price": 32, "Cost": 64},
+            {"Item": "Bananas", "Category": "Fruits/Food", "Qty": 4, "Price": 8, "Cost": 32}
+        ],
+        "Total": 106
+    }
+}
